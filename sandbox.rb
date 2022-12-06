689e5ed0a717e37f265f8afe9c05f059bf0820eb
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2022-12-06 12:08:04 +0800

    Enable awesome_print gem. Played with Rules Engine's establishing of facts.

--- sandbox.rb
+++ sandbox.rb
@@ -1,8 +1,12 @@
-require 'wongi-engine'
+require "wongi-engine"
+require "awesome_print"
+
 engine = Wongi::Engine.create
 
 engine << ["Alice", :friend, "Bob"]
 engine << ["Alice", :age, 35]
+
+ap engine.entity("Alice")
 
 # this how you display the facts
 engine.entity("Alice").each do |predicate, object|
