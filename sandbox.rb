d0c8c7304bf3cdf86ffeb33658ae8219790bd17b
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2022-12-06 13:49:41 +0800

    WP: Working version of the rule.

--- sandbox.rb
+++ sandbox.rb
@@ -6,16 +6,30 @@ require_relative "rules_engine.rb"
 
 puts "1:08pm"
 pawn = Pawn.new
-ap pawn.placed = true
-ap pawn
+pawn.color = "white"
+pawn.placed = true
+
 
 rules_engine = RulesEngine.instance
-rules_engine << ["Alice", :friend, "Bob"]
-rules_engine << ["Alice", :age, 35]
+rules_engine << [pawn, :placed, true]
+rules_engine << [pawn, :color, "white"]
 
-ap rules_engine.entity("Alice")
+ap rules_engine.entity(pawn)
 
 # this how you display the facts
-rules_engine.entity("Alice").each do |predicate, object|
-  puts "Alice's #{predicate} is #{object}"
+rules_engine.entity(pawn).each do |predicate, object|
+  puts "Pawn #{predicate} is #{object}"
 end
+
+pawn_placed_rule = rules_engine.rule "Pawn placed onto board" do
+  forall {
+    has :A, :placed, :B
+  }
+  make {
+    action { |token|
+      puts "%s and %s" % [ token[:A], token[:B] ]
+    }
+  }
+end
+
+
