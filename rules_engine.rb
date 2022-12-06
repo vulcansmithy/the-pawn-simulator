52454329a877e94d9d6bf23af96a6a070ec4bb03
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2022-12-06 13:10:17 +0800

    Establish an initial RulesEngine.

---
+++ rules_engine.rb
@@ -0,0 +1,9 @@
+class RulesEngine
+  def initialize
+    raise 'Singleton class cannot be initialized'
+  end
+
+  def self.instance
+    @instance ||= Wongi::Engine.create
+  end
+end
--- sandbox.rb
+++ sandbox.rb
@@ -2,19 +2,20 @@ require "wongi-engine"
 require "awesome_print"
 
 require_relative "pawn.rb"
+require_relative "rules_engine.rb"
 
-engine = Wongi::Engine.create
-
-engine << ["Alice", :friend, "Bob"]
-engine << ["Alice", :age, 35]
-
-ap engine.entity("Alice")
-
-# this how you display the facts
-engine.entity("Alice").each do |predicate, object|
-  puts "Alice's #{predicate} is #{object}"
-end
-
+puts "1:08pm"
 pawn = Pawn.new
 ap pawn.placed = true
 ap pawn
+
+rules_engine = RulesEngine.instance
+rules_engine << ["Alice", :friend, "Bob"]
+rules_engine << ["Alice", :age, 35]
+
+ap rules_engine.entity("Alice")
+
+# this how you display the facts
+rules_engine.entity("Alice").each do |predicate, object|
+  puts "Alice's #{predicate} is #{object}"
+end
