e320f4e8af729ff0596ee1f05013f078e0c5be30
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2022-12-10 02:53:06 +0800

    Fix failing rspec test.

--- spec/models/chessboard_rspec.rb
+++ spec/models/chessboard_rspec.rb
@@ -5,34 +5,32 @@ require 'awesome_print'
 
 RSpec.describe "Chessboard", type: :model do
   describe "border check" do
-    it "should check if on the border" do
-      pawn = Pawn.new
-      pawn.heading = :south
-      expect( Chessboard.check_if_within_the_boarder(pawn)).to eq true
-    end
-
     it "should not move forward if the heading is north and at the north border" do
       pawn = Pawn.new
       pawn.place_on_the_board(rand(0..7), 7, :north, :white)
-      expect(pawn.move).to eq false
+      pawn.move
+      expect(pawn.y_position).to eq 7
     end 
 
     it "should not move forward if the heading is east and at the east border" do
       pawn = Pawn.new
       pawn.place_on_the_board(7, rand(0..7), :east, :white)
-      expect(pawn.move).to eq false
+      pawn.move
+      expect(pawn.x_position).to eq 7
     end
 
     it "should not move forward if the heading is south and at the south border" do
       pawn = Pawn.new
       pawn.place_on_the_board(rand(0..7), 0, :south, :white)
-      expect(pawn.move).to eq false
+      pawn.move
+      expect(pawn.y_position).to eq 0
     end  
 
     it "should not move forward if the heading is west and at the west border" do
       pawn = Pawn.new
       pawn.place_on_the_board(0, rand(0..7), :west, :white)
-      expect(pawn.move).to eq false
+      pawn.move
+      expect(pawn.x_position).to eq 0
     end 
   end
 end
