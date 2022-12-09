652b589ed8da669266e6f1d9646829216ae6cc58
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2022-12-09 22:41:17 +0800

    Code refactor.

--- sandbox.rb
+++ sandbox.rb
@@ -7,30 +7,29 @@ require "awesome_print"
 def command_processor(cmd)
   result = nil
   processed_cmd = nil
+  [
+    /EXIT/,
+    /LEFT/,
+    /MOVE/,
+    /PLACE/,
+    /REPORT/,
+    /RIGHT/
+  ].each do |recognized_command|
+    unless (result = cmd.scan(recognized_command)).empty?
+      processed_cmd = result.first
+      break
+    end  
+  end  
 
-  if (result = cmd.scan(/EXIT/)).empty? == false
-    processed_cmd = result.first
-  elsif (result = cmd.scan(/LEFT/)).empty? == false
-    processed_cmd = result.first
-  elsif (result = cmd.scan(/MOVE/)).empty? == false
-    processed_cmd = result.first
-  elsif (result = cmd.scan(/PLACE/)).empty? == false   
-    processed_cmd = result.first
-  elsif (result = cmd.scan(/REPORT/)).empty? == false 
-    processed_cmd = result.first
-  elsif (result = cmd.scan(/RIGHT/)).empty? == false  
-    processed_cmd = result.first 
-  end
-  
-  return processed_cmd 
+  return processed_cmd
 end    
 
 
 while true do
-    print ">> "
-    cmd = gets.chomp.upcase
+    print ":: "
+    raw_cmd = gets.chomp.upcase
 
-    processed_cmd = command_processor(cmd)
+    processed_cmd = command_processor(raw_cmd)
     puts "\"#{processed_cmd}\"" if processed_cmd != nil
 
     exit if processed_cmd == "EXIT"
